e641dee471fae2edb8cab3c98bf551df6bc09544
ahmedmohsinnazir <ahmeddmohsinnazir@gmail.com>
2025-02-13 16:57:07 +0500

    Add files via upload

---
+++ First Class/FA24BCSB.java
@@ -0,0 +1,9 @@
+public class FA24BCSB
+{
+    public static void main(String[] args)
+    {
+        System.out.println("First Java Task");
+	System.out.print("First Java Task");
+	System.out.printf("First Java Task");
+    }   
+}
