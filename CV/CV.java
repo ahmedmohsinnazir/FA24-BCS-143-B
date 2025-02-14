25b03745d658be7a80d7c4b12b1efd4573ebe88f
ahmedmohsinnazir <ahmeddmohsinnazir@gmail.com>
2025-02-14 10:51:43 +0500

    Delete CV directory

--- CV/CV.java
+++
@@ -1,9 +0,0 @@
-public class CV
-{
-	public static void main(String args[])
-	{
-		System.out.println("Name: Ahmed Mohsin Nazir");
-		System.out.print("Age: 19");
-		System.out.printf("University: COMSATS");	
-	}
-}
