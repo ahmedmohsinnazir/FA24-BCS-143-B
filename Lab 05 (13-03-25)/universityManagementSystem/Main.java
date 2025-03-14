7706e2f4e6d6575ded6fe83b3cffc534cc7bd1cb
ahmedmohsinnazir <ahmeddmohsinnazir@gmail.com>
2025-03-14 12:49:51 +0500

    Update Main.java

--- Lab 05 (13-03-25)/universityManagementSystem/Main.java
+++ Lab 05 (13-03-25)/universityManagementSystem/Main.java
@@ -4,49 +4,49 @@ public class Main
 	{
         	Person director = new Person("Dr. Syed Asad Hussain", "Director");
 
-        Department[] departments = new Department[10];
-        departments[0] = new Department("Computer Science", "Muhammad Ammar", "HOD");
-        departments[1] = new Department("Architecture", "Asif Khan", "HOD");
-        departments[2] = new Department("Management Sciences", "Ayesha Ali", "HOD");
+        	Department[] departments = new Department[10];
+        	departments[0] = new Department("Computer Science", "Muhammad Ammar", "HOD");
+        	departments[1] = new Department("Architecture", "Asif Khan", "HOD");
+        	departments[2] = new Department("Management Sciences", "Ayesha Ali", "HOD");
         
-	Lab[] labs = new Lab[20];
-        for (int i = 0; i < 5; i++) 
-	{
-        	Person labStaff = new Person("Farooq Khan", "Lab Attendant");
-            	labs[i] = new Lab("Lab " + (i + 1), labStaff);
+		Lab[] labs = new Lab[20];
+        	for (int i = 0; i < 5; i++) 
+		{
+        		Person labStaff = new Person("Farooq Khan", "Lab Attendant");
+            		labs[i] = new Lab("Lab " + (i + 1), labStaff);
             
-            	labs[i].numOfPC = new PC[50];
-            	for (int j = 0; j < 10; j++) 
-		{
-                labs[i].numOfPC[j] = new PC("PC" + (j + 1), "Intel i7", 16, 512);
-         	}
-        }
+            		labs[i].numOfPC = new PC[50];
+            		for (int j = 0; j < 10; j++) 
+			{
+                		labs[i].numOfPC[j] = new PC("PC" + (j + 1), "Intel i7", 16, 512);
+         		}
+        	}
         
-        for (int i = 0; i < 3; i++) 
-	{
-        	if (departments[i] != null) 
+        	for (int i = 0; i < 3; i++) 
 		{
-                	departments[i].labs = labs;
-            	}
-        }
+        		if (departments[i] != null) 
+			{
+                		departments[i].labs = labs;
+            		}
+        	}	
         
-        University myUniversity = new University("COMSATS University", departments, "Dr. Syed Asad Hussain", 
-	"Director", "Defence Road", "Lahore");
+        	University myUniversity = new University("COMSATS University", departments, "Dr. Syed Asad Hussain", 
+		"Director", "Defence Road", "Lahore");
 
-        myUniversity.displayUniversityDetails();
-        for (Department dept : departments) 
-	{
-        	if (dept != null) 
+        	myUniversity.displayUniversityDetails();
+        	for (Department dept : departments) 
 		{
-                	dept.displayDepartmentDetails();
-            	}
-        }
-        for (Lab lab : labs) 
-	{
-        	if (lab != null) 
+        		if (dept != null) 
+			{
+                		dept.displayDepartmentDetails();
+            		}
+        	}
+        	for (Lab lab : labs) 
 		{
-                	lab.displayLabDetails();
-            	}
-        }
-    }
+        		if (lab != null) 
+			{
+                		lab.displayLabDetails();
+            		}
+        	}
+    	}
 }
